d3f9594337e987fad83d339417deb631e731093b
岡本紗也加 <impl@implnoMacBook-Pro-5.local>
2022-11-18 10:16:01 +0900

    ADD: ベースレイアウトを作成する

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
+<style>
+body {font-size: 16pt; color:#999;}
+h1 {font-size: 100pt; text-align:right; color:#eee;
+  margin:-40px 0px -50px 0px;}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></head>';
+
+function tag($tag, $txt) {
+  return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
+class HelloController extends Controller
+{
+  public function index()
+  {
+    $data = ['one', 'two', 'three', 'four', 'five'];
+    return view('hello.index', ['data'=>$data]);
+  }
+
+  public function post(Request $request)
+  {
+    return view('hello.index', ['msg'=>$request->msg]);
+  }
+}
